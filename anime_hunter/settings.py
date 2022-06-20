b20bd20606b1ef8fc34218cca276e52d59439583
GabrielSouzaCosta <gabrielsscosta2010@hotmail.com>
2022-06-20 07:52:24 -0300

    pwd

--- anime_hunter/settings.py
+++ anime_hunter/settings.py
@@ -27,6 +27,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.environ.get('SECRET_KEY')
+DATABASE_PWD = os.environ.get('DATABASE_PWD')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -89,7 +90,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'anime_hunter',
         'USER': 'gabrielsc',
-        'PASSWORD': env('DATABASE_PWD'),
+        'PASSWORD': DATABASE_PWD,
         'HOST': 'localhost',
         'PORT': '5432',
     }

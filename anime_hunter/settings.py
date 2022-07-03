639958138777b4b8c378e73886cdb7a9ee31438f
GabrielSouzaCosta <gabrielsscosta2010@hotmail.com>
2022-07-03 12:46:49 -0300

    debug

--- anime_hunter/settings.py
+++ anime_hunter/settings.py
@@ -31,7 +31,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 DATABASE_PWD = os.environ.get('DATABASE_PWD')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 

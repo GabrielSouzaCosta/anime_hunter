c0c1685358749a6ebc5255d44c5a96b25658a0a2
GabrielSouzaCosta <gabrielsscosta2010@hotmail.com>
2022-07-03 16:04:10 -0300

    migrate

--- anime_hunter/settings.py
+++ anime_hunter/settings.py
@@ -91,17 +91,17 @@ import dj_database_url
 
 
 DATABASES = {
-    # 'default': {
-    #     'ENGINE': 'django.db.backends.postgresql',
-    #     'NAME': os.environ.get('DATABASE_NAME'),
-    #     'USER': os.environ.get('DATABASE_USERNAME'),
-    #     'PASSWORD': os.environ.get('DATABASE_PWD'),
-    #     'HOST': os.environ.get('DATABASE_HOST'),
-    #     'PORT': 5432,
-    # },
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.environ.get('DATABASE_NAME'),
+        'USER': os.environ.get('DATABASE_USERNAME'),
+        'PASSWORD': os.environ.get('DATABASE_PWD'),
+        'HOST': os.environ.get('DATABASE_HOST'),
+        'PORT': 5432,
+    },
 }
 
-DATABASES['default'] = dj_database_url.config()
+# DATABASES['default'] = dj_database_url.config()
 
 
 # Password validation

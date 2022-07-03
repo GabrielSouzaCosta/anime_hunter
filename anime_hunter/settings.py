88b2c9d40d4ae68e55598acd02b21afe58f27436
GabrielSouzaCosta <gabrielsscosta2010@hotmail.com>
2022-07-03 15:51:40 -0300

    ..

--- anime_hunter/settings.py
+++ anime_hunter/settings.py
@@ -27,7 +27,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.environ.get('SECRET_KEY')
-DATABASE_PWD = os.environ.get('DATABASE_PWD')
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get('DEBUG')
